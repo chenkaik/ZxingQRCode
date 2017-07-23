36b3cd14d34d62b4693775073b6292c9b0b81d81
chenkai <chenkai_k7@163.com>
2017-07-23 16:35:09 +0800

    first commit

---
+++ app/src/main/java/com/example/chenkai/zxingqrcode/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.chenkai.zxingqrcode;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.afun.zxinglib.EncodingUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editText;
+    private ImageView imageView;
+
+    private CameraManager manager;
+    public static boolean kaiguan = true; // 定义开关状态，状态为false，打开状态，状态为true，关闭状态
+
+//    private boolean status = false;
+//    private Camera camera;
+//    private Camera.Parameters parameters;
+//    private MainActivity instance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_s).setOnClickListener(this);
+        findViewById(R.id.shengcheng).setOnClickListener(this);
+        findViewById(R.id.open).setOnClickListener(this);
+        editText = (EditText) findViewById(R.id.editText);
+        imageView = (ImageView) findViewById(R.id.imageview);
+        // 闪光灯
+//        instance = this;
+//        camera = Camera.open();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(this,QrScanActivity.class));
+                } else {
+                    Toast.makeText(this, "请允许程序访问相机权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_s:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                }else {
+                    startActivity(new Intent(this,QrScanActivity.class));
+                }
+                break;
+            case R.id.open:
+//                    if (!status) {
+//                        status = true;
+//                        new Thread(new TurnOnLight()).start();
+//                    }else {
+//                        status = false;
+//                        //openLight.setText("打开");
+//                        instance.parameters.setFlashMode("off");
+//                        instance.camera.setParameters(instance.parameters);
+//                    }
+                break;
+            case R.id.shengcheng:
+                String info = editText.getText().toString().trim();
+                if (TextUtils.isEmpty(info)){
+                    Toast.makeText(this, "内容不能为空", Toast.LENGTH_SHORT).show();
+                }else {
+                    Bitmap b = EncodingUtils.createQRCode(info,500,500, BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launr));
+                    if (b != null){
+                        imageView.setImageBitmap(b);
+                    }
+                }
+            default:
+                break;
+        }
+    }
+
+
+//    private class TurnOnLight implements Runnable{
+//        @Override
+//        public void run() {
+//            instance.parameters = instance.camera.getParameters();
+//            instance.parameters.setFlashMode("torch");
+//            instance.camera.setParameters(instance.parameters);
+//        }
+//    }
+
+    /**
+     * 判断Android系统版本是否 >= LOLLIPOP(API21)
+     *
+     * @return boolean
+     */
+    private boolean isLOLLIPOP() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/chenkai/zxingqrcode/QrScanActivity.java
@@ -0,0 +1,114 @@
+package com.example.chenkai.zxingqrcode;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.afun.zxinglib.BeepManager;
+import com.afun.zxinglib.ScanView.ZXingScannerViewNew;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QrScanActivity extends AppCompatActivity implements ZXingScannerViewNew.ResultHandler, ZXingScannerViewNew.QrSize {
+    ZXingScannerViewNew scanView;
+    private TextView result;
+    private BeepManager beepManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 初始化自己定制的扫描界面
+        scanView = new ZXingScannerViewNew(this);
+        scanView.setContentView(R.layout.logistics_scan_qr);
+        scanView.setQrSize(this);
+        setContentView(scanView);
+        beepManager = new BeepManager(this);
+        setupFormats();
+        initViews();
+        result= (TextView) findViewById(R.id.editText);
+    }
+
+    private void initViews() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 设置处理扫描结果的回调方法
+        scanView.setResultHandler(this);
+        // 设置camera
+        scanView.startCamera(-1);
+        scanView.setFlash(false);
+        scanView.setAutoFocus(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanView.stopCamera();
+        beepManager.close();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        beepManager.playBeepSoundAndVibrate();
+        // 回调方法中接收扫描结果并且设置到textview中  可判断扫描结果是否为null
+        result.setText(rawResult.toString());
+    }
+
+    // 可以解析二维码和条形码等，我们这里只添加二维码格式，让他只处理二维码
+    public void setupFormats() {
+        List<BarcodeFormat> formats = new ArrayList<BarcodeFormat>();
+        formats.add(BarcodeFormat.QR_CODE);
+        /**
+        formats.add(BarcodeFormat.UPC_A);
+        formats.add(BarcodeFormat.UPC_E);
+        formats.add(BarcodeFormat.EAN_13);
+        formats.add(BarcodeFormat.EAN_8);
+        formats.add(BarcodeFormat.CODE_39);
+        formats.add(BarcodeFormat.CODE_93);
+        formats.add(BarcodeFormat.CODE_128);
+        formats.add(BarcodeFormat.ITF);
+         */
+        if (scanView != null) {
+            scanView.setFormats(formats);
+        }
+    }
+
+    @Override
+    public Rect getDetectRect() {
+        View view = findViewById(R.id.scan_window);
+        int top = ((View) view.getParent()).getTop() + view.getTop();
+        int left = view.getLeft();
+        int width = view.getWidth();
+        int height = view.getHeight();
+        Rect rect = null;
+        if (width != 0 && height != 0) {
+            rect = new Rect(left, top, left + width, top + height);
+            addLineAnim(rect);
+        }
+        return rect;
+    }
+
+    // 给扫描框内的绿线设置移动动画，让它在扫描区域内循环移动
+    private void addLineAnim(Rect rect) {
+        ImageView imageView = (ImageView) findViewById(R.id.scanner_line);
+        imageView.setVisibility(View.VISIBLE);
+        if (imageView.getAnimation() == null) {
+            TranslateAnimation anim = new TranslateAnimation(0, 0, 0, rect.height());
+            anim.setDuration(2000);
+            anim.setRepeatCount(Animation.INFINITE);
+            imageView.startAnimation(anim);
+        }
+    }
+
+}
